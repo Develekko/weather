b4aa4033e2789e26bd1f1f30b61fe76d1d4dcd01
Housam Ramadan <52341115+Develekko@users.noreply.github.com>
2022-10-12 16:43:52 +0200

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,8 +13,6 @@ const monthNames = ["January", "February", "March", "April", "May", "June", "Jul
 const d = new Date();
 
 
-
-
 document.getElementById("search").addEventListener("input", e => {
     search(e.target.value)
     if(e.target.value == "")
@@ -40,13 +38,15 @@ async function search(term) {
 
 function displayCurrent()
 {
+    
+
     currentTime.classList.add("animate__animated","animate__fadeInDown","animate__delay-1s");
     weatherCondition();
     let term  = `
     <img class="img-fluid weatherIcon animate__animated animate__fadeIn animate__delay-1s" src="${dataCurrent.condition.icon}" alt="">
     <h2 class="h1 weatherText animate__animated animate__fadeIn animate__delay-2s">${dataCurrent.condition.text}</h2>
     <h5 class="weatherLocation animate__animated animate__fadeIn animate__delay-3s">${dataLocation.name} , ${dataLocation.country} </h5>
-    <h2 class="h1 animate__animated animate__fadeIn animate__delay-4s temperature">${dataCurrent.temp_c} °C</h2>
+    <h2 temp="C" class="h1 animate__animated animate__fadeIn animate__delay-4s temperature">${dataCurrent.temp_c} °C</button></h2>
     <h5 class="animate__animated animate__fadeIn animate__delay-5s">${days[d.getDay()]}</h5>
     <h5 class="animate__animated animate__fadeIn animate__delay-6s">${d.getDate() +" "+ monthNames[d.getMonth()]}</h5>
     `
@@ -63,7 +63,7 @@ function displayCurrent()
         <div class="col-10 col-xl-7 col-xxl-6 col-sm-9  p-0 ps-2">
             <div class="weather-details-caption">
                 <p class="m-0">Humidity</p>
-                <h2>${dataCurrent.humidity} %</h2>
+                <h2 class="humidityAlert">${dataCurrent.humidity} %</h2>
             </div>
         </div>
     </div>
@@ -116,7 +116,7 @@ function displayCurrent()
         <div class="col-10 col-xl-7 col-xxl-6 col-sm-9  p-0 ps-2">
             <div class="weather-details-caption">
                 <p class="m-0">Wind Speed</p>
-                <h2 id="windSpeed" >${dataCurrent.wind_kph} <span class="h4">km/h</span></h2>
+                <h2 id="windSpeed">${dataCurrent.wind_kph} <span class="h4">km/h</span></h2>
             </div>
         </div>
         </div>
@@ -127,8 +127,8 @@ function displayCurrent()
 </div>
     </div>
     `
-    generalWeather.innerHTML = term
-    weatherInfo.innerHTML = term2
+    generalWeather.innerHTML = term;
+    weatherInfo.innerHTML = term2;
 }
 function weatherCondition()
 {
@@ -178,6 +178,7 @@ function weatherCondition()
         document.body.style.background = 'url("assets/images/fog.jpg") center center / cover fixed'
     }
 }
+
 function myTimer() 
 {
     currentTime.innerHTML = new Date().toLocaleTimeString( 'en-US', { timeZone: `${dataLocation.tz_id}` });
@@ -204,7 +205,7 @@ function displayForecast(dataForecast)
         <h2 class="h3 forcastTempMax"><i class="bi bi-thermometer-high"></i> ${dataForecast[i].day.maxtemp_c}°C</h2>
         <h2 class=" my-2 h5 forcastTempMin"><i class="bi bi-thermometer-low text-info"></i> ${dataForecast[i].day.mintemp_c}°C</h2>
         <h2 class="h3 forcastMois"><i class="bi bi-moisture"></i> ${dataForecast[i].day.avghumidity} %</h2>
-        <h2 class="my-2 h3 forcastWind"><i class="bi bi-wind"></i> ${dataForecast[i].day.maxwind_kph}km/h</h2>
+        <h2 class="my-2 h3 forcastWind"><i class="bi bi-wind text-white"></i> ${dataForecast[i].day.maxwind_kph}km/h</h2>
         <h2 class="h3 forcastRain"> <i class="bi bi-cloud-drizzle"></i> ${dataForecast[i].day.daily_chance_of_rain} %</h2>
         <h2 class="mt-1 h3 forcastUv">${dataForecast[i].day.uv} UV</h2>
         </div>
@@ -214,6 +215,89 @@ function displayForecast(dataForecast)
         </div>
         `
     }
-    forecastWeather.innerHTML = term
+    forecastWeather.innerHTML = term;
+    weatherAlert(dataForecast);
 }
+
+function weatherAlert()
+{
+    for (let i = 1; i < dataForecast.length; i++) {
+        if(dataForecast[i].day.maxwind_kph < 1)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#fff");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 6)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#66FFFF");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 12)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#00FFFF");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 20)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#00FF99");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 29)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#66FF66");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 39)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#99FF32");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 50)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#CDFF32");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 62)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#FFFF00");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 75)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#FFC100");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 89)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#FF9900");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 103)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#FF6600");
+        }
+        else if(dataForecast[i].day.maxwind_kph < 118)
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#FF3200");
+        }
+        else
+        {
+            document.querySelectorAll('.forcastWind').forEach((e, i) => e.style.color = "#FF0000");
+        }
+
+        if(dataForecast[i].day.uv < 3)
+        {
+            document.querySelectorAll('.forcastUv').forEach((e, i) => e.style.color = "#43B91E");
+        }
+        else if(dataForecast[i].day.uv < 6)
+        {
+            document.querySelectorAll('.forcastUv').forEach((e, i) => e.style.color = "#FCC721");
+        }
+        else if(dataForecast[i].day.uv < 8)
+        {
+            document.querySelectorAll('.forcastUv').forEach((e, i) => e.style.color = "#FB741B");
+        }
+        else if(dataForecast[i].day.uv < 11)
+        {
+            document.querySelectorAll('.forcastUv').forEach((e, i) => e.style.color = "#F81116");
+        }
+        else
+        {
+            document.querySelectorAll('.forcastUv').forEach((e, i) => e.style.color = "#000");
+        }
+    }
+
+}
+
+
 
